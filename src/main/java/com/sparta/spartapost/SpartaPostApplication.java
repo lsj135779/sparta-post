05f8dcd280785edab9010dd18d6c0ef244685414
이승준 <lsj135779@gmail.com>
2023-11-05 18:06:02 +0900

    초기 설정

---
+++ src/main/java/com/sparta/spartapost/SpartaPostApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartapost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaPostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spartapost/SpartaPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartapost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaPostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
